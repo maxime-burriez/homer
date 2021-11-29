3578664ca16b1a12870d71f29733de3e1956f625
Maxime Burriez <burriez_maxime@hotmail.com>
2021-11-29 01:13:42 +0100

    :sparkles: Add Homer.Search.Server (GenServer) to search Offers [step 6]

---
+++ lib/homer/search/server_behaviour.ex
@@ -0,0 +1,14 @@
+defmodule Homer.Search.ServerBehaviour do
+  @callback start_link({OfferRequest.t(), providers :: [module()]}) ::
+              {:ok, pid()} | {:error, any()}
+
+  @doc """
+  Get the filtered and sorted list of offers.
+  """
+  @callback list_offers(pid :: pid(), limit :: integer()) :: {:ok, [Offer.t()]} | {:error, any()}
+
+  @doc """
+  Notify the server that an offer request has been updated.
+  """
+  @callback offer_request_updated(pid :: pid(), OfferRequest.t()) :: :ok
+end
