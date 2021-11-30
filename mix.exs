4c2a3f75a30d8261907e2d1837067240dc30d9e4
Maxime Burriez <burriez_maxime@hotmail.com>
2021-11-30 11:08:30 +0100

    :wrench: Add "test/module_mocks" to paths for compilation in :test environment. :bug: Fix Homer.Search.DuffelMock.fetch_offers/1 to respond with a tuple {:ok, [Offer.t()]} | {:error, any()}

--- mix.exs
+++ mix.exs
@@ -25,7 +25,13 @@ defmodule Homer.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test),
+    do: [
+      "lib",
+      "test/module_mocks",
+      "test/support"
+    ]
+
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
